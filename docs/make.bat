fafe19d7b3e39e5e034cfbe52b6233e75aa2e9a9
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2016-07-21 23:19:30 -0400

    Fix conf.py script

--- docs/make.bat
+++ docs/make.bat
@@ -127,9 +127,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\ISBCancerGenomicsCloud.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\mediator.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\ISBCancerGenomicsCloud.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\mediator.ghc
 	goto end
 )
 
